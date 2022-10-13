fa39e0e849371e805d20ecdff705c01888af52a8
DjabaTina <djabaaugustina1@gmail.com>
2022-10-13 23:20:10 +0100

    server side props

--- pages/students/[studentId].js
+++ pages/students/[studentId].js
@@ -1,22 +1,9 @@
 import React from "react";
 import { getStudents } from "../../utils/getStudents";
 
-export async function getStaticPaths() {
-  const res = await getStudents();
 
-  const paths = res.map((student) => ({
-    params: { studentId: string(student._id) },
-  }));
 
-  console.log(paths);
-
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   let student = {};
   try {
     student = await getStudents(context.params.studentId);
